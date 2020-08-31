9a8be668ea2433a6ef384d58c403b089c0e66647
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-08-31 10:44:32 -0600

    Removed images added links

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -1,13 +1,22 @@
 import styles from "./Card.module.css";
+import Link from "next/link";
 
 const Card = (props) => {
+    let target = "_blank";
+    if (props.href.startsWith("/")) {
+        target = "";
+    }
+
     return (
         <div className={styles.Card}>
             <div className={styles.Title}>{props.title}</div>
-            <img className={styles.Image} src={props.img} />
-            <div className={styles.DescriptionBlock}>
-                <div className={styles.Description}>{props.description}</div>
-                <div>{props.children}</div>
+            <div className={styles.Description}>{props.description}</div>
+            <div className={styles.LinkBlock}>
+                <div className={styles.Link}>
+                    <Link href={props.href}>
+                        <a target={target}>{props.hrefTitle}</a>
+                    </Link>
+                </div>
             </div>
         </div>
     );
