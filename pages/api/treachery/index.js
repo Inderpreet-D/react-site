c218e641961e0a073d4d30cee5963ddac7695c92
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-09-02 20:30:03 -0600

    Start Treachery API

---
+++ pages/api/treachery/index.js
@@ -0,0 +1,11 @@
+import rooms from "./state";
+
+export default (req, res) => {
+    console.log(res.query);
+    // state.counter++;
+    // res.statusCode = 200;
+    // res.setHeader("Content-Type", "application/json");
+    // res.send(
+    //     JSON.stringify({ name: "Treachery MTG Endpoint", count: state.counter })
+    // );
+};
