e3ee42c29af493f383bca3c1675936d984477364
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-09-06 19:33:06 -0600

    Uses material ui now

--- containers/Twilio/Chat/Chat.jsx
+++ containers/Twilio/Chat/Chat.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import styles from "./Chat.module.css";
 import Message from "./Message";
@@ -7,7 +7,7 @@ import { socket } from "./socket";
 const MY_NAME = "Inderpreet";
 const PHONE_NUMBER = "+1 (205) 896 - 2409";
 
-const Chat = (props) => {
+const Chat = () => {
     const [message, setMessage] = useState("");
     const [messages, setMessages] = useState([]);
 
@@ -56,8 +56,8 @@ const Chat = (props) => {
     const boxClass = styles.MessageBox + " BorderedBox";
 
     return (
-        <Fragment>
-            <div className="BorderedBox">Twilio Test App</div>
+        <div className={styles.Border}>
+            <div>Twilio Test App</div>
             <div className={boxClass}>
                 Text your name to <b>{PHONE_NUMBER}</b> to start
             </div>
@@ -73,7 +73,7 @@ const Chat = (props) => {
                     />
                 </form>
             </div>
-        </Fragment>
+        </div>
     );
 };
 
