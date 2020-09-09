9eff5c9e8f2c969a58c10a82b3883d7e55700828
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-09-09 15:35:27 -0600

    Uses Page

--- pages/404.js
+++ pages/404.js
@@ -1,22 +1,21 @@
-import { Fragment } from "react";
 import Link from "next/link";
-import Head from "next/head";
+import { Paper } from "@material-ui/core";
 
+import Page from "../components/Page";
 import styles from "../styles/Error.module.css";
 
 const Custom404 = () => {
     return (
-        <Fragment>
-            <Head>
-                <title>404 - Page Not Found</title>
-            </Head>
+        <Page title="404 - Page Not Found">
             <div className={styles.Error}>
-                <h1>Page Not Found</h1>
-                <Link href="/" replace>
-                    <a>Go back home</a>
-                </Link>
+                <Paper variant="outlined">
+                    <h1>Page Not Found</h1>
+                    <Link href="/" replace>
+                        <a className={styles.Link}>Go back home</a>
+                    </Link>
+                </Paper>
             </div>
-        </Fragment>
+        </Page>
     );
 };
 
