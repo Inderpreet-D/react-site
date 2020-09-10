dc38c437dbf103a95d7a7ff52545b5605cbbd695
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-09-10 00:58:18 -0600

    Disabled for now

--- containers/Twilio/Chat/socket.js
+++ containers/Twilio/Chat/socket.js
@@ -1,4 +1,8 @@
-import io from "socket.io-client";
-const endpoint = "http://localhost:4000";
+// import io from "socket.io-client";
+// const endpoint = "http://localhost:4000";
 
-export const socket = io(endpoint);
+export const socket = {
+    on: () => {},
+    off: () => {},
+    emit: () => {},
+}; //io(endpoint);
--- pages/twilio.js
+++ pages/twilio.js
@@ -2,7 +2,11 @@ import Page from "../components/Page";
 import TwilioChat from "../containers/Twilio/Chat";
 
 const Twilio = (props) => {
-    return <Page title="Twilio Test App">{/* <TwilioChat /> */}</Page>;
+    return (
+        // <Page title="Twilio Test App">
+        <TwilioChat />
+        // </Page>
+    );
 };
 
 export default Twilio;
