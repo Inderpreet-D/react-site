fe3be93bfe2ff506f95609f7315a89b511c45c20
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-09-10 00:39:08 -0600

    Started room creation logic

--- pages/api/treachery/create.js
+++ pages/api/treachery/create.js
@@ -1,22 +1,37 @@
-import rooms from "./state";
+import { addRoom, getRooms } from "./state";
 import { send } from "./index";
 
 const getUniqueRoomCode = () => {
-    let code;
+    let code = "";
     for (let i = 0; i < 4; i++) {
         code += String.fromCharCode(65 + Math.floor(Math.random() * 26));
     }
 
-    if (code in rooms) {
+    if (code in getRooms()) {
         return getUniqueRoomCode();
+    } else {
+        return code;
     }
-    return code;
+};
+
+const getCards = (numPlayers, rarity) => {
+    return ["asd", "evgh", "asfdg"];
 };
 
 export default (req, res) => {
     const { numPlayers, rarity } = req.query;
-    const result = {};
-    const roomCode = getUniqueRoomCode();
-    result.roomCode = roomCode;
-    send(res, result);
+
+    if (!numPlayers || !rarity) {
+        send(res, { error: "Missing params for room creation" });
+    } else {
+        const roomCode = getUniqueRoomCode();
+
+        addRoom(roomCode, {
+            numPlayers: numPlayers,
+            rarity: rarity,
+            cards: getCards(numPlayers, rarity),
+        });
+
+        send(res, { roomCode: roomCode });
+    }
 };
