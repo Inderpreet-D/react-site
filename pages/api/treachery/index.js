8d2b5262677d04f68e60ef2d5c09facc4f4f6fc6
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-09-09 20:49:29 -0600

    Update index.js

--- pages/api/treachery/index.js
+++ pages/api/treachery/index.js
@@ -1,7 +1,12 @@
 import rooms from "./state";
 
 export default (req, res) => {
-    console.log(res.query);
+    const { roomCode } = req.query;
+    console.log("Requested room", roomCode);
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.send(JSON.stringify({ name: "Treachery MTG Endpoint" }));
+
     // state.counter++;
     // res.statusCode = 200;
     // res.setHeader("Content-Type", "application/json");
