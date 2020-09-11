a0c0c5785e3fe01f2cff71fa3a893de286f6ca87
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-09-11 17:32:45 -0600

    Changed data handling to a flat file db

--- pages/api/treachery/index.js
+++ pages/api/treachery/index.js
@@ -1,9 +1,87 @@
-export const send = (res, data) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.send(JSON.stringify(data));
+import fs from "fs";
+
+import * as helpers from "./helper";
+
+const ROOM_PATH = "public/treacheryRooms.json";
+
+const readRooms = () => {
+  return JSON.parse(fs.readFileSync(ROOM_PATH));
+};
+
+const writeRooms = (rooms) => {
+  fs.writeFileSync(ROOM_PATH, JSON.stringify(rooms));
+};
+
+const handleRoomCreation = (payload, rooms) => {
+  const { numPlayers, rarity } = payload;
+
+  const roomCode = helpers.getUniqueRoomCode(rooms);
+  const id = helpers.getUniqueRoomCode({});
+
+  rooms[roomCode] = {
+    numPlayers: +numPlayers,
+    currentPlayers: 1,
+    cards: helpers.getCards(numPlayers, rarity),
+    ids: { [id]: 0 },
+    nextIDX: 1,
+  };
+
+  writeRooms(rooms);
+
+  return { roomCode: roomCode, id: id };
+};
+
+const handleRoomJoin = (payload, rooms) => {
+  const { roomCode, id } = payload;
+
+  if (!(roomCode in rooms)) {
+    return { error: "Room not found" };
+  }
+
+  const ids = rooms[roomCode].ids;
+  if (!id) {
+    const newId = helpers.getUniqueRoomCode(ids);
+    rooms[roomCode].ids[newId] = rooms[roomCode].nextIDX;
+    rooms[roomCode].nextIDX++;
+    rooms[roomCode].currentPlayers++;
+    writeRooms(rooms);
+    return {
+      roomCode: roomCode,
+      id: newId,
+      currentPlayers: rooms[roomCode].currentPlayers,
+      numPlayers: rooms[roomCode].numPlayers,
+    };
+  }
+
+  return {
+    roomCode: roomCode,
+    id: id,
+    currentPlayers: rooms[roomCode].currentPlayers,
+    numPlayers: rooms[roomCode].numPlayers,
+  };
+};
+
+const handleRoomPing = (payload, rooms) => {
+  const { roomCode } = payload;
+  const { numPlayers, currentPlayers } = rooms[roomCode];
+  return { numPlayers: numPlayers, currentPlayers: currentPlayers };
 };
 
 export default (req, res) => {
-    send(res, { rooms: rooms });
+  const { action, ...payload } = req.query;
+  const rooms = readRooms();
+
+  let result;
+  if (!action) {
+    result = rooms;
+  } else if (action === "create") {
+    result = handleRoomCreation(payload, rooms);
+    console.log("Created", rooms);
+  } else if (action === "join") {
+    result = handleRoomJoin(payload, rooms);
+  } else if (action === "room") {
+    result = handleRoomPing(payload, rooms);
+  }
+
+  helpers.send(res, result);
 };
