e6483d7de797ae2a8f8390ebea3c3ae8f0dfa5ee
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-09-11 00:48:59 -0600

    Update LoadingIcon.jsx

--- components/LoadingIcon/LoadingIcon.jsx
+++ components/LoadingIcon/LoadingIcon.jsx
@@ -1,20 +1,13 @@
-import { useEffect, useState } from "react";
-
 import styles from "./LoadingIcon.module.css";
 
 const LoadingIcon = () => {
-    const [colors, setColors] = useState([
+    const colors = [
         "rgb(0, 115, 62)", // Green
         "rgb(211, 32, 42)", // Red
         "rgb(21, 11, 0)", // Black
         "rgb(14, 104, 171)", // Blue
         "rgb(249, 250, 244)", // White
-    ]);
-
-    setTimeout(() => {
-        const last = colors[colors.length - 1];
-        setColors([last, ...colors.slice(0, 4)]);
-    }, 2000);
+    ];
 
     return (
         <div className={styles.Wrapper}>
