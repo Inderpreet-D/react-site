cee5ae60774555763d7f6cab79b7ee097de5ddec
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-09-11 01:07:23 -0600

    Update treachery.js

--- pages/treachery.js
+++ pages/treachery.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Paper, makeStyles } from "@material-ui/core";
 import axios from "axios";
 
@@ -23,6 +23,7 @@ const STATES = {
     Main: 0,
     Room: 1,
     Card: 2,
+    Loading: 3,
 };
 
 const Treachery = () => {
@@ -34,53 +35,85 @@ const Treachery = () => {
     const [role, setRole] = useState("");
     const [imgSrc, setImgSrc] = useState("/favicon.ico");
     const [winCondition, setWinCondition] = useState("");
+    const [error, setError] = useState(null);
 
     const onJoinHandler = (roomCode) => {
+        setState(STATES.Loading);
+        setError(null);
         console.log(`Trying to join ${roomCode}`);
-        axios.get("/api/treachery").then((res) => {
-            console.log(res.data);
+
+        axios.get(`/api/treachery/join?roomCode=${roomCode}`).then((res) => {
+            const data = res.data;
+            if (data.error) {
+                setError(data.error);
+                setState(STATES.Main);
+            } else {
+                setRoomCode(roomCode);
+                setState(STATES.Room);
+            }
         });
     };
 
     const onCreateHandler = (numPlayers, rarity) => {
-        setState(-1);
+        setState(STATES.Loading);
+        setError(null);
+
         axios
             .get(
                 `/api/treachery/create?numPlayers=${numPlayers}&rarity=${rarity}`
             )
             .then((res) => {
-                console.log(res.data);
-                setState(STATES.Main);
+                const data = res.data;
+                if (data.error) {
+                    setError(data.error);
+                    setState(STATES.Main);
+                } else {
+                    setRoomCode(data.roomCode);
+                    setState(STATES.Room);
+                    setNumPlayers(1);
+                    setRoomSize(numPlayers);
+                }
             });
     };
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (state === STATES.Room) {
+                axios
+                    .get(`/api/treachery/room?roomCode=${roomCode}`)
+                    .then((res) => {
+                        const data = res.data;
+                        setNumPlayers(data.currentPlayers);
+                        setRoomSize(data.numPlayers);
+                    });
+            }
+        }, 5000);
+        return () => {
+            clearInterval(interval);
+        };
+    }, [state]);
+
     let page;
-    switch (state) {
-        case STATES.Main:
-            page = (
-                <Main
-                    onJoin={onJoinHandler}
-                    onCreate={onCreateHandler}
-                    forwardClasses={classes}
-                />
-            );
-            break;
-        case STATES.Room:
-            page = (
-                <Room
-                    roomCode={roomCode}
-                    numPlayers={numPlayers}
-                    roomSize={roomSize}
-                />
-            );
-            break;
-        case STATES.Card:
-            page = (
-                <Card role={role} imgSrc={imgSrc} winCondition={winCondition} />
-            );
-            break;
-        default:
-            page = <LoadingIcon />;
+    if (state === STATES.Main) {
+        page = (
+            <Main
+                onJoin={onJoinHandler}
+                onCreate={onCreateHandler}
+                forwardClasses={classes}
+            />
+        );
+    } else if (state === STATES.Room) {
+        page = (
+            <Room
+                roomCode={roomCode}
+                numPlayers={numPlayers}
+                roomSize={roomSize}
+            />
+        );
+    } else if (state === STATES.Card) {
+        page = <Card role={role} imgSrc={imgSrc} winCondition={winCondition} />;
+    } else {
+        page = <LoadingIcon />;
     }
 
     return (
@@ -89,6 +122,11 @@ const Treachery = () => {
                 <h1>MTG Treachery</h1>
             </div>
             <Paper variant="outlined" className={classes.root}>
+                {error && (
+                    <div>
+                        <h3 style={{ color: "red" }}>Error: {error}</h3>
+                    </div>
+                )}
                 {page}
             </Paper>
         </Page>
