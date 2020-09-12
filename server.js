066e23a656f47dc6e3b92130a8e3001afee5cd54
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-09-11 21:59:55 -0600

    Redirect works

--- server.js
+++ server.js
@@ -13,22 +13,15 @@ app
     const server = express();
 
     server.use((req, res, next) => {
-      // const hostname =
-      // req.hostname === "www.inderpreetd.ca" ? "inderpreetd.ca" : req.hostname;
-
-      if (
-        req.headers["x-forwarded-proto"] === "http" //||
-        // req.hostname === "www.inderpreetd.ca"
-      ) {
+      if (req.headers["x-forwarded-proto"] === "http") {
         res.redirect(301, `https://${req.hostname}${req.url}`);
-        return;
+      } else {
+        res.setHeader(
+          "strict-transport-security",
+          "max-age=31536000; includeSubDomains; preload"
+        );
+        next();
       }
-
-      res.setHeader(
-        "strict-transport-security",
-        "max-age=31536000; includeSubDomains; preload"
-      );
-      next();
     });
 
     server.get("*", (req, res) => handle(req, res));
