833be414b4dfefb014907b875b93bd44aac605f3
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-09-12 19:48:58 -0600

    Switched to material ui

--- pages/secret.jsx
+++ pages/secret.jsx
@@ -1,37 +1,66 @@
 import { useState } from "react";
+import { Paper, Typography, TextField, makeStyles } from "@material-ui/core";
 
-import styles from "../styles/Secret.module.css";
 import Page from "../components/Page";
 import { part1, part2, part3, decode } from "../secret-helper";
 
+const useStyles = makeStyles(() => ({
+  root: {
+    "& > *": {
+      wordBreak: "break-word",
+    },
+    margin: "10px auto 0 auto",
+    width: "95%",
+    textAlign: "center",
+    padding: "10px",
+  },
+  text: {
+    marginTop: "10px",
+    padding: "10px",
+    textAlign: "left",
+  },
+  form: {
+    width: "100%",
+    textAlign: "center",
+    paddingTop: "20px",
+  },
+  textField: {
+    width: "50%",
+  },
+}));
+
 const Secret = () => {
+  const classes = useStyles();
   const [userInput, setUserInput] = useState("");
-  const nextClasses = [styles.BorderedBox, styles.BorderedNext].join(" ");
 
   return (
     <Page title="Secret">
-      <div className={styles.BorderedBox}>
-        <p className={styles.TopText}>{decode(userInput, part1)}</p>
-      </div>
-      <div className={nextClasses}>
-        <p className={styles.MidText}>{decode(userInput, part2)}</p>
-      </div>
-      <div className={nextClasses}>
-        <p className={styles.BotText}>{decode(userInput, part3)}</p>
-      </div>
-      <div className={nextClasses}>
-        <form
-          className={styles.Input}
-          onSubmit={(event) => event.preventDefault()}
+      <Paper variant="outlined" className={classes.root}>
+        <Typography variant="h1">{decode(userInput, part1)}</Typography>
+        <Typography variant="h6" className={classes.text}>
+          {decode(userInput, part2)}
+        </Typography>
+        <Typography
+          variant="h7"
+          className={classes.text}
+          style={{ textAlign: "center" }}
         >
-          <input
-            className={styles.InputElement}
-            value={userInput}
-            onChange={(event) => setUserInput(event.target.value)}
+          {decode(userInput, part3)}
+        </Typography>
+        <form
+          onSubmit={(event) => event.preventDefault()}
+          className={classes.form}
+        >
+          <TextField
+            variant="filled"
+            color="secondary"
             placeholder="Who are you to me?"
+            onChange={(event) => setUserInput(event.target.value)}
+            value={userInput}
+            className={classes.textField}
           />
         </form>
-      </div>
+      </Paper>
     </Page>
   );
 };
