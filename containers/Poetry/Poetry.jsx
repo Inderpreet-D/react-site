5bff7321a18d8341b7f8e09829c9b1942bdd6aa3
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-09-13 00:30:50 -0600

    Update Poetry.jsx

--- containers/Poetry/Poetry.jsx
+++ containers/Poetry/Poetry.jsx
@@ -1,9 +1,41 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import parse from "html-react-parser";
+
 const Poetry = () => {
-  const poemName = "Test Name";
+  const [poemName, setPoemName] = useState("");
+  const [poemBody, setPoemBody] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://cors-anywhere.herokuapp.com/https://www.reddit.com/r/poetry/hot.json"
+      )
+      .then((res) => {
+        const allPosts = res.data.data.children;
+        const textPosts = allPosts.filter((listing) => {
+          const post = listing.data;
+          const isPoem = post.title.startsWith("[POEM]");
+          const hasText = post.selftext.length > 0;
+          return isPoem && hasText;
+        });
+        const poems = textPosts.map((listing) => {
+          console.log(listing);
+          const poem = listing.data;
+          const title = poem.title.substring(7);
+          return { name: title.trim(), body: unescape(poem.selftext_html) };
+        });
+        const idx = Math.floor(Math.random() * poems.length);
+        const poem = poems[idx];
+        setPoemName(poem.name);
+        setPoemBody(poem.body);
+      });
+  }, []);
 
   return (
     <div style={{ textAlign: "center" }}>
       <h1>{poemName}</h1>
+      <div dangerouslySetInnerHTML={{ __html: parse(poemBody) }}></div>
     </div>
   );
 };
