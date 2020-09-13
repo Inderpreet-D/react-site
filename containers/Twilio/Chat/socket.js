85c261bdad4928316ac7f24a097a0c1f5d9539b0
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-09-12 21:24:39 -0600

    Update socket.js

--- containers/Twilio/Chat/socket.js
+++ containers/Twilio/Chat/socket.js
@@ -1,8 +1,9 @@
 // import io from "socket.io-client";
 // const endpoint = "http://localhost:4000";
+// export const socket = io(endpoint);
 
 export const socket = {
   on: () => {},
   off: () => {},
   emit: () => {},
-}; //io(endpoint);
+};
