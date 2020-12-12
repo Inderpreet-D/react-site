0b863e2b0d4eebb74669ddaa6c58d7649a32b32b
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-12-12 13:52:20 -0700

    Fixed UI

--- pages/secret.jsx
+++ pages/secret.jsx
@@ -1,23 +1,41 @@
-import { useState } from "react";
-import { Paper, Typography, TextField, makeStyles } from "@material-ui/core";
+import React from "react";
+import Head from "next/head";
+import { Paper, TextField, makeStyles } from "@material-ui/core";
 
-import Page from "../components/Page";
-import { part1, part2, part3, decode } from "../utilities/secret-helper.js";
+import { part1, midParts, part3, decode } from "../utilities/secret-helper.js";
 
 const useStyles = makeStyles(() => ({
   root: {
-    "& > *": {
-      wordBreak: "break-word",
-    },
-    margin: "10px auto 0 auto",
-    width: "95%",
+    width: "100vw",
+    height: "100vh",
+    overflow: "hidden",
+  },
+  paper: {
+    margin: "10px",
+    width: "calc(100% - 20px)",
+    height: "calc(100% - 20px)",
     textAlign: "center",
     padding: "10px",
+    display: "flex",
+    flexDirection: "column",
   },
-  text: {
+  textBlock: {
+    fontFamily: "Courier New",
     marginTop: "10px",
     padding: "10px",
+    wordBreak: "break-word",
+  },
+  title: {
+    fontSize: "75px",
+  },
+  text: {
     textAlign: "left",
+    fontSize: "20px",
+    flexShrink: "1",
+    overflowY: "auto",
+  },
+  subtext: {
+    fontSize: "15px",
   },
   form: {
     width: "100%",
@@ -25,43 +43,49 @@ const useStyles = makeStyles(() => ({
     paddingTop: "20px",
   },
   textField: {
-    width: "50%",
+    width: "60%",
   },
 }));
 
 const Secret = () => {
   const classes = useStyles();
-  const [userInput, setUserInput] = useState("");
+  const [userInput, setUserInput] = React.useState("");
 
+  const textClass = (c) => `${c} ${classes.textBlock}`;
   return (
-    <Page title="Secret">
-      <Paper variant="outlined" className={classes.root}>
-        <Typography variant="h1">{decode(userInput, part1)}</Typography>
-        <Typography variant="h6" className={classes.text}>
-          {decode(userInput, part2)}
-        </Typography>
-        <Typography
-          variant="h7"
-          className={classes.text}
-          style={{ textAlign: "center" }}
-        >
-          {decode(userInput, part3)}
-        </Typography>
-        <form
-          onSubmit={(event) => event.preventDefault()}
-          className={classes.form}
-        >
-          <TextField
-            variant="filled"
-            color="secondary"
-            placeholder="Who are you to me?"
-            onChange={(event) => setUserInput(event.target.value)}
-            value={userInput}
-            className={classes.textField}
-          />
-        </form>
-      </Paper>
-    </Page>
+    <>
+      <Head>
+        <title>Secret</title>
+      </Head>
+      <div className={classes.root}>
+        <Paper variant="outlined" className={classes.paper}>
+          <div className={textClass(classes.title)}>
+            {decode(userInput, part1)}
+          </div>
+
+          {midParts.map((part, i) => (
+            <div key={i} className={textClass(classes.text)}>
+              {decode(userInput, part)}
+            </div>
+          ))}
+
+          <div className={textClass(classes.subtext)}>
+            {decode(userInput, part3)}
+          </div>
+
+          <form onSubmit={(e) => e.preventDefault()} className={classes.form}>
+            <TextField
+              variant="filled"
+              color="secondary"
+              placeholder="Who are you to me?"
+              onChange={(e) => setUserInput(e.target.value)}
+              value={userInput}
+              className={classes.textField}
+            />
+          </form>
+        </Paper>
+      </div>
+    </>
   );
 };
 
