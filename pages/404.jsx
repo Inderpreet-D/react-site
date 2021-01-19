98360fd7dec37ddd980fe050260305fd74feeb87
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2021-01-18 23:38:57 -0700

    Working on error

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,13 +1,21 @@
 import Link from "next/link";
 import { Paper } from "@material-ui/core";
+import styled from "styled-components";
 
 import Page from "../components/Page";
 import styles from "../styles/Error.module.css";
+
+const Test = styled.div`
+  background-color: ${(props) => props.theme.colors.primary};
+  width: 40px;
+  height: 200%;
+`;
 
 const Custom404 = () => {
   return (
     <Page title="404 - Page Not Found">
       <div className={styles.Error}>
+        <Test>Anything</Test>
         <Paper variant="outlined">
           <h1>Page Not Found</h1>
           <Link href="/" replace>
