4f28b2f9a2a7fa5ebe1256865410588e5664f33c
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2021-01-20 10:37:26 -0700

    Fixed styling for error page

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,28 +1,39 @@
 import Link from "next/link";
-import { Paper } from "@material-ui/core";
 import styled from "styled-components";
 
 import Page from "../components/Page";
-import styles from "../styles/Error.module.css";
 
-const Test = styled.div`
+const ErrorBox = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  background-color: ${({ theme }) => theme.backgroundLight};
   border: ${({ theme }) => `0.125rem solid ${theme.foregroundDark}`};
-  width: 40px;
-  height: 200%;
+  padding: 1.25rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledLink = styled.a`
+  color: ${({ theme }) => theme.foreground};
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+  }
 `;
 
 const Custom404 = () => {
   return (
     <Page title="404 - Page Not Found">
-      <div className={styles.Error}>
-        <Test>Anything</Test>
-        <Paper variant="outlined">
-          <h1>Page Not Found</h1>
-          <Link href="/" replace>
-            <a className={styles.Link}>Go back home</a>
-          </Link>
-        </Paper>
-      </div>
+      <ErrorBox>
+        <h1>Page Not Found</h1>
+        <Link href="/" replace>
+          <StyledLink>Go back home</StyledLink>
+        </Link>
+      </ErrorBox>
     </Page>
   );
 };
