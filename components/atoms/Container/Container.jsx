c15451aa6d16a9666f47141321b433b1fd250eae
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2021-01-30 17:54:11 -0700

    Added some breakpoints

--- components/atoms/Container/Container.jsx
+++ components/atoms/Container/Container.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import breakpoints from "../../../breakpoints";
+
 export default styled.div`
   width: 55%;
   margin: 1rem auto;
@@ -7,4 +9,14 @@ export default styled.div`
   border: ${({ theme }) => `0.125rem solid ${theme.foregroundDark}`};
   border-radius: 1rem;
   background-color: ${({ theme }) => theme.backgroundLight};
+
+  @media ${breakpoints.mobile} {
+    width: 80%;
+  }
+  @media ${breakpoints.tablet} {
+    width: calc(55% + (2 * calc(25% / 3)));
+  }
+  @media ${breakpoints.laptop} {
+    width: calc(55% + calc(25% / 3));
+  }
 `;
