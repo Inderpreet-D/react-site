032418a2153c38e2fb47c8aad56f92e2570e857a
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2021-01-30 17:55:26 -0700

    Fixed desktop query

--- breakpoints.js
+++ breakpoints.js
@@ -2,5 +2,5 @@ export default {
   mobile: "(min-width: 20rem) and (max-width: 30rem)",
   tablet: "(min-width: 30.0625rem) and (max-width: 48rem)",
   laptop: "(min-width: 48.0625rem) and (max-width: 64rem)",
-  desktop: "(min-width: 64.0625rem) and (max-width: 75rem)",
+  desktop: "(min-width: 64.0625rem)",
 };
