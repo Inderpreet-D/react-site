1bc551003db3f08dcb8958c9a2af127b88c73bf8
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2021-02-15 22:47:26 -0700

    Finished Date component

--- components/pages/Home/Sections/Date/Date.jsx
+++ components/pages/Home/Sections/Date/Date.jsx
@@ -1,7 +1,79 @@
 import styled from "styled-components";
+import { GiFrayedArrow } from "react-icons/gi";
 
-const Date = ({ data, idx }) => (
-  <div>Date {JSON.stringify({ data, idx }, null, 2)}</div>
-);
+const StyledArrow = styled(GiFrayedArrow)`
+  align-items: center;
+  justify-content: center;
+
+  display: flex;
+  transform: rotate(-45deg);
+
+  margin-right: 0.75rem;
+`;
+
+const StyledPoint = styled.div`
+  display: flex;
+
+  margin-bottom: 0.75rem;
+`;
+
+const StyledPoints = styled.div`
+  flex-direction: column;
+
+  display: flex;
+
+  padding: 1rem;
+`;
+
+const StyledHeader = styled.div`
+  display: flex;
+
+  & > span:first-child {
+    flex-grow: 1;
+
+    margin-bottom: 0.5rem;
+
+    font-weight: bold;
+  }
+
+  & > span:last-child {
+    font-style: italic;
+  }
+`;
+
+const StyledContainer = styled.div`
+  flex-direction: column;
+
+  display: flex;
+
+  padding: 1rem;
+`;
+
+const Date = ({ data, idx }) => {
+  const { name, location, title, date, points } = data[idx];
+
+  return (
+    <StyledContainer>
+      <StyledHeader>
+        <span>{name}</span>
+        <span>{location}</span>
+      </StyledHeader>
+
+      <StyledHeader>
+        <span>{title}</span>
+        <span>{date}</span>
+      </StyledHeader>
+
+      <StyledPoints>
+        {points.map((point, i) => (
+          <StyledPoint key={i}>
+            <StyledArrow />
+            {point}
+          </StyledPoint>
+        ))}
+      </StyledPoints>
+    </StyledContainer>
+  );
+};
 
 export default Date;
