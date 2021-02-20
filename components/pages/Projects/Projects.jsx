08a8eadc79c170fcb4dc7704ef3b4e967d624bda
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2021-02-20 10:31:41 -0700

    Fixed consistency

--- components/pages/Projects/Projects.jsx
+++ components/pages/Projects/Projects.jsx
@@ -1,12 +1,10 @@
 import Page from "../../templates/Page";
 import ProjectsPage from "./Page";
 
-const Projects = () => {
-  return (
-    <Page title="Projects">
-      <ProjectsPage />
-    </Page>
-  );
-};
+const Projects = () => (
+  <Page title="Projects">
+    <ProjectsPage />
+  </Page>
+);
 
 export default Projects;
