bac1764fa91028e25e0361dea393cd42f874fd10
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2021-02-20 10:34:20 -0700

    Fixed styles

--- components/templates/Page/Page.jsx
+++ components/templates/Page/Page.jsx
@@ -1,21 +1,8 @@
-import styled from "styled-components";
 import Head from "next/head";
-import PropTypes from "prop-types";
 
+import { StyledWrapper, StyledChildren } from "./Page.styles";
 import Header from "../../atoms/Header";
 import Footer from "../../atoms/Footer";
-
-const StyledWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100vw;
-  height: 100vh;
-`;
-
-const StyledChildren = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`;
 
 const Page = ({ children, title, hideHeader, hideFooter }) => (
   <>
@@ -32,9 +19,5 @@ const Page = ({ children, title, hideHeader, hideFooter }) => (
     </StyledWrapper>
   </>
 );
-
-Page.propTypes = {
-  title: PropTypes.string.isRequired,
-};
 
 export default Page;
