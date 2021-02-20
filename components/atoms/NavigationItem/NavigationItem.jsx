eb71a11ac271a3e4353489f1030a335444653250
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2021-02-20 01:58:28 -0700

    Styling

--- components/atoms/NavigationItem/NavigationItem.jsx
+++ components/atoms/NavigationItem/NavigationItem.jsx
@@ -1,50 +1,7 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-const StyledHolder = styled.div`
-  height: 3.125rem;
-  width: 100%;
-  cursor: pointer;
-  user-select: none;
-`;
-
-const StyledLink = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  padding: 0 0.25rem;
-  height: 100%;
-  width: 100%;
-  box-sizing: border-box;
-  border: 0.25rem solid transparent;
-  transition: all 0.75s;
-
-  &:hover,
-  &:hover,
-  &.active {
-    color: ${({ theme }) => theme.foreground};
-  }
-
-  @media ${breakpoints.laptop}, ${breakpoints.desktop} {
-    &:hover,
-    &:active,
-    &.active {
-      background-color: ${({ theme }) => theme.background};
-      border-bottom: 0.25rem solid ${({ theme }) => theme.foregroundDark};
-    }
-  }
-
-  @media ${breakpoints.mobile}, ${breakpoints.tablet} {
-    &:hover,
-    &:active,
-    &.active {
-      background-color: ${({ theme }) => theme.backgroundLight};
-      border-right: 0.25rem solid ${({ theme }) => theme.foregroundDark};
-      border-left: 0.25rem solid ${({ theme }) => theme.foregroundDark};
-    }
-  }
-`;
+import { StyledHolder, StyledLink } from "./NavigationItem.styles";
 
 const NavigationItem = ({ link, children }) => {
   const { pathname } = useRouter();
--- components/atoms/NavigationItem/NavigationItem.styles.jsx
+++ components/atoms/NavigationItem/NavigationItem.styles.jsx
@@ -1,3 +1,58 @@
 import styled from "styled-components";
 
 import breakpoints from "../../../breakpoints";
+
+const StyledHolder = styled.div`
+  width: 100%;
+  height: 3.125rem;
+
+  cursor: pointer;
+
+  user-select: none;
+`;
+
+const StyledLink = styled.div`
+  justify-content: center;
+  align-items: center;
+
+  display: flex;
+
+  transition: all 0.75s;
+
+  border: 0.25rem solid transparent;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  padding: 0 0.25rem;
+
+  text-align: center;
+
+  &:hover,
+  &:hover,
+  &.active {
+    color: ${({ theme }) => theme.foreground};
+  }
+
+  @media ${breakpoints.laptop}, ${breakpoints.desktop} {
+    &:hover,
+    &:active,
+    &.active {
+      border-bottom: 0.25rem solid ${({ theme }) => theme.foregroundDark};
+
+      background-color: ${({ theme }) => theme.background};
+    }
+  }
+
+  @media ${breakpoints.mobile}, ${breakpoints.tablet} {
+    &:hover,
+    &:active,
+    &.active {
+      border-right: 0.25rem solid ${({ theme }) => theme.foregroundDark};
+      border-left: 0.25rem solid ${({ theme }) => theme.foregroundDark};
+
+      background-color: ${({ theme }) => theme.backgroundLight};
+    }
+  }
+`;
+
+export { StyledHolder, StyledLink };
