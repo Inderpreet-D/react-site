94ac3551f0a254fe5aa535badba2d805d40f9c60
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2021-02-19 19:56:42 -0700

    Fixed button styling

--- components/atoms/Button/Button.jsx
+++ components/atoms/Button/Button.jsx
@@ -1,51 +1,13 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
-const resetStyles = css`
-  display: inline-block;
-  margin: 0;
-  text-decoration: none;
-  font-family: inherit;
-  font-size: 1rem;
-  text-align: center;
-  cursor: pointer;
-  appearance: none;
-  outline: none;
-`;
-
-const StyledButton = styled.button`
-  ${resetStyles};
-
-  padding: 0.5rem 1rem;
-  border-radius: 0.75rem;
-  border: 0.0625rem solid ${({ theme }) => theme.foreground};
-  color: ${({ theme }) => theme.text};
-  background-color: ${({ theme }) => theme.backgroundLight};
-  transition: color 250ms ease-in-out, background-color 250ms ease-in-out,
-    transform 150ms ease;
-
-  &:hover,
-  &:focus {
-    color: black;
-    background-color: ${({ theme }) => theme.foregroundDark};
-  }
-
-  &:active :not(:disabled) {
-    transform: scale(0.95);
-  }
-
-  &:disabled {
-    cursor: not-allowed;
-    color: ${({ theme }) => theme.background};
-    background-color: ${({ theme }) => theme.accent};
-    border-color: gray;
-  }
-`;
+import StyledButton from "./Button.styles";
 
 const Button = ({ onClick, ...props }) => {
   const btnRef = React.useRef();
 
   const handleClick = (e) => {
     btnRef.current?.blur();
+
     if (onClick) {
       e.preventDefault();
       onClick(e);
