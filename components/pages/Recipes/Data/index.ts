f5dc0f522bab80cd49be41db16d53ef28aedcc7e
Inderpreet Dhillon <4023685+Inderpreet-D@users.noreply.github.com>
2022-01-01 16:55:53 -0700

    Added more recipes (#15)

--- components/pages/Recipes/Data/index.ts
+++ components/pages/Recipes/Data/index.ts
@@ -1,9 +1,11 @@
 import { Recipe, Recipes } from '../types'
 
-import BrownButterCookies from './brownButterCookies'
+// import BrownButterCookies from './brownButterCookies'
+import BrownButterCookies from './brownButterCookies2'
 import FudgyBrownies from './fudgyBrownies'
+import SuperBrownies from './superBrownies'
 
-const recipes: Recipe[] = [BrownButterCookies, FudgyBrownies]
+const recipes: Recipe[] = [BrownButterCookies, FudgyBrownies, SuperBrownies]
 
 const asObj: Recipes = recipes.reduce((prev, curr) => {
   prev[curr.title] = curr
--- components/pages/Recipes/Data/superBrownies.ts
+++ components/pages/Recipes/Data/superBrownies.ts
@@ -1,12 +1,13 @@
 import { Ingredient, Recipe, Page } from '../types'
 
 const ingredients: Ingredient[] = [
-  { amount: '290g', type: 'Unsalted butter, cubed' },
-  { amount: '500g', type: 'Sugar' },
-  { amount: '160g', type: 'Unsweetened cocoa powder' },
-  { amount: '1/2tsp', type: 'Kosher salt' },
+  { amount: '226g', type: 'Unsalted butter, cubed' },
+  { amount: '227g', type: 'Bitter-sweet chocolate' },
+  { amount: '1/2tsp', type: 'Salt' },
+  { amount: '200g', type: 'Sugar' },
+  { amount: '230g', type: 'Brown sugar' },
   { amount: '2tsp', type: 'Vanilla extract' },
-  { amount: '4', type: 'Large eggs' },
+  { amount: '4', type: 'Large Eggs' },
   { amount: '130g', type: 'All-purpose flour' }
 ]
 
@@ -14,33 +15,33 @@ const pages: Page[] = [
   [
     'Setup a double boiler by adding a bit of water to a pot and resting a heat-safe bowl over it.',
     'Make sure the water is just simmering and not touching the bowl.',
-    'To this bowl add the butter, sugar, cocoa powder, and salt.',
-    'Let this mixture heat and stir occasionally until the butter has melted.'
+    'To this bowl add the butter and chocolate.',
+    'Let this mixture heat and stir occasionally until the butter and chocolate have melted.'
   ],
   [
     'Take the bowl off the heat and let it cool for a few minutes.',
-    'Now is a good time to let the oven preheat to 325°F and line your baking pan with parchment paper.',
+    'Now is a good time to let the oven preheat to 350°F and line your baking pan with parchment paper.',
     'Make sure the parchment goes over the sides of the pan to make getting the brownies out later easier.'
   ],
   [
     'The bowl should only be warm instead of hot now.',
-    'Stir in the vanilla.',
+    'Stir in the salt, sugars, and vanilla.',
     'Add the eggs one at a time and mix well after each one.',
     'When the batter looks thick and shiny then add the flour.',
     'Keep mixing until the batter pulls away from the sides of the bowl.'
   ],
   [
     'Spread the batter evenly in your pan.',
-    'Bake for 20-25 minutes or until a toothpick inserted in the center comes out clean.'
+    'Bake for 45 minutes or until a toothpick inserted in the center comes out clean.'
   ]
 ]
 
-const fudgyBrownies: Recipe = {
-  title: 'Fudgy Brownies',
-  bake: '325°F for 20-25 minutes',
-  makes: '16 brownies, in a 9x13 pan, half the recipe to fit a 8x8',
+const superBrownies: Recipe = {
+  title: 'Super Brownies',
+  bake: '350°F for 45 minutes',
+  makes: '16 brownies in a 9x13 pan',
   ingredients,
   pages
 }
 
-export default fudgyBrownies
+export default superBrownies
