30b55b7e7c7da814ffa1f97101b1c8cc061f969e
Inderpreet Dhillon <4023685+Inderpreet-D@users.noreply.github.com>
2022-01-04 23:05:58 -0700

    Integrated markdown

--- components/pages/Competitive/Rules/index.tsx
+++ components/pages/Competitive/Rules/index.tsx
@@ -1,8 +1,12 @@
+import Markdown from '../../../atoms/Markdown'
+
 type RulesProps = {
   rules: string[]
 }
 
 type RuleType = 'BAN' | 'GENERAL'
+
+const TYPE_SEPARATOR = '::'
 
 const Rules: React.FC<RulesProps> = ({ rules }) => {
   return (
@@ -10,12 +14,12 @@ const Rules: React.FC<RulesProps> = ({ rules }) => {
       <div>Rules</div>
 
       {rules.map((rule, i) => {
-        const [t, text] = rule.split('::')
+        const [t, text] = rule.split(TYPE_SEPARATOR)
         const type = t as RuleType
 
         return (
           <div key={i} style={{ color: type === 'BAN' ? 'red' : 'white' }}>
-            {text}
+            <Markdown markdown={text} />
           </div>
         )
       })}
