fec94249664a74de9542ed6ffeece103d5784072
Inderpreet Dhillon <4023685+Inderpreet-D@users.noreply.github.com>
2022-02-21 04:06:12 -0700

    Update index.ts

--- components/pages/Projects/Data/index.ts
+++ components/pages/Projects/Data/index.ts
@@ -34,19 +34,19 @@ const projects: Project[] = [
   //   href: '/mtgdb',
   //   hrefTitle: 'Go to MagicDB'
   // },
-  {
-    title: 'ART 321 Site',
-    description: 'My website for a Net Art course I took in University.',
-    href: 'http://pages.cpsc.ucalgary.ca/~inderpreet.dhillon/ART321/index.html',
-    hrefTitle: 'View ART 321'
-  },
-  {
-    title: 'SENG 513 Site',
-    description: 'Projects for a course in web applications.',
-    href:
-      'http://pages.cpsc.ucalgary.ca/~inderpreet.dhillon/SENG513/index.html',
-    hrefTitle: 'View SENG 513'
-  },
+  // {
+  //   title: 'ART 321 Site',
+  //   description: 'My website for a Net Art course I took in University.',
+  //   href: 'http://pages.cpsc.ucalgary.ca/~inderpreet.dhillon/ART321/index.html',
+  //   hrefTitle: 'View ART 321'
+  // },
+  // {
+  //   title: 'SENG 513 Site',
+  //   description: 'Projects for a course in web applications.',
+  //   href:
+  //     'http://pages.cpsc.ucalgary.ca/~inderpreet.dhillon/SENG513/index.html',
+  //   hrefTitle: 'View SENG 513'
+  // },
   {
     title: 'Treachery App',
     description:
