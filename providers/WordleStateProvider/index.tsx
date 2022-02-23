9817b535265ad32c35be206ab2dcf49c7b9126ad
Inderpreet Dhillon <4023685+Inderpreet-D@users.noreply.github.com>
2022-02-22 19:12:51 -0700

    Update index.tsx

--- providers/WordleStateProvider/index.tsx
+++ providers/WordleStateProvider/index.tsx
@@ -19,10 +19,6 @@ const WordleStateProvider: React.FC = ({ children }) => {
     dispatch({ type: 'MAKE_GUESS', guess })
   }, [])
 
-  React.useEffect(() => {
-    console.log({ state })
-  }, [state])
-
   return (
     <WordleStateContext.Provider value={{ state, startGame, makeGuess }}>
       {children}
