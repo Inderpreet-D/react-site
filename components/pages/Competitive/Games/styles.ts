6eeb2c256ce6032e5193faaab4d65dfd13e0bb77
Inderpreet Dhillon <4023685+Inderpreet-D@users.noreply.github.com>
2022-02-22 18:04:23 -0700

    Fixed styling for games list (#22)

--- components/pages/Competitive/Games/styles.ts
+++ components/pages/Competitive/Games/styles.ts
@@ -1,5 +1,7 @@
 import styled, { css } from 'styled-components'
 
+import breakpoints from '../../../../utilities/breakpoints'
+
 export const TableHolder = styled.div`
   overflow-x: auto;
 `
@@ -44,3 +46,21 @@ export const Row = styled.tr<TRProps>`
 export const HeaderCell = styled.th``
 
 export const DataCell = styled.td``
+
+export const GameGrid = styled.div`
+  align-content: space-between;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+
+  display: grid;
+
+  padding: 2rem;
+
+  @media ${breakpoints.tablet} {
+    grid-template-columns: auto auto;
+  }
+
+  @media ${breakpoints.base}, ${breakpoints.mobile} {
+    grid-template-columns: auto;
+  }
+`
