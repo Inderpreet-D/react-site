b0d02237bd0c9105835fd138f56bf383bb180cc8
Inderpreet Dhillon <4023685+Inderpreet-D@users.noreply.github.com>
2022-04-05 20:40:49 -0600

    Removed flip, fixed responsiveness

--- components/molecules/MTGCard/styles.tsx
+++ components/molecules/MTGCard/styles.tsx
@@ -2,38 +2,29 @@ import styled, { css } from 'styled-components'
 
 const noCardStyles = css`
   border-color: red;
-
   color: red;
 `
 
 const greyStyles = css`
   border-color: gray;
-
   background-color: ${({ theme }) => theme.accent};
   cursor: not-allowed;
-
   color: gray;
 
   &:hover {
-    transform: scale(1);
-
     border-color: gray;
-
     background-color: ${({ theme }) => theme.accent};
-
+    transform: scale(1);
     color: gray;
   }
 `
 
 const StyledCard = styled.div`
-  flex-direction: column;
-
   display: flex;
-
+  flex-direction: column;
   margin: 0.25rem;
   border: 0.125rem solid ${({ theme }) => theme.foregroundDark};
   border-radius: 0.5rem;
-
   background-color: ${({ theme }) => theme.backgroundLight};
 `
 
@@ -44,17 +35,12 @@ const StyledCardCount = styled.div<CardCountProps>`
   position: absolute;
   top: 30%;
   left: 50%;
-
-  transform: translateX(-50%) translateY(-50%);
-
-  transition: opacity 1s ease-in-out;
-
   border: 0.0625rem solid ${({ theme }) => theme.accent};
-  border-radius: 50%;
+  border-radius: 100rem;
   padding: 0.5rem;
-
   background-color: ${({ theme }) => theme.backgroundLight};
-
+  transform: translateX(-50%) translateY(-50%);
+  transition: opacity 400ms ease-in-out;
   font-size: 3rem;
   line-height: 3rem;
   font-weight: bold;
@@ -64,11 +50,9 @@ const StyledCardCount = styled.div<CardCountProps>`
 `
 
 const StyledImageHolder = styled.div`
-  justify-content: center;
-
+  position: relative;
   display: flex;
-  perspective: 20rem;
-
+  justify-content: center;
   width: 100%;
 
   &:hover ${StyledCardCount} {
@@ -80,53 +64,29 @@ const StyledImageHolder = styled.div`
   }
 `
 
-type FlippingCardProps = {
-  flipped: boolean
-}
-const StyledFlippingCard = styled.div<FlippingCardProps>`
+const StyledCardImageHolder = styled.div`
   position: relative;
-
-  transform: ${({ flipped }) => flipped && 'rotateY(180deg)'};
-  transform-style: preserve-3d;
-
-  transition: transform 1s;
-
   width: 95%;
-  height: 20rem;
 `
 
-type CardImageProps = {
-  isBack?: boolean
-}
-const StyledCardImage = styled.img<CardImageProps>`
-  position: absolute;
-  top: 0;
-  left: 0;
-
-  transform: ${({ isBack }) => isBack && 'rotateY(180deg)'};
-  backface-visibility: hidden;
-
-  transition: transform 1s;
-
-  margin: 0.3rem auto;
+const StyledCardImage = styled.img`
+  transition: transform 400ms;
+  margin: 0.5rem auto 0.3rem auto;
   width: 100%;
   border: 1px solid ${({ theme }) => theme.background};
-  border-radius: 0.75rem;
+  border-radius: 1rem;
   box-sizing: border-box;
 
   &:hover {
-    transform: scale(1.5)
-      ${({ isBack }) => (isBack ? 'rotateY(180deg)' : 'rotateY(0)')};
-
+    transform: scale(1.25);
     cursor: zoom-in;
   }
 `
 
 const StyledCardActions = styled.div`
+  display: flex;
   justify-content: space-around;
   align-items: center;
-  display: flex;
-
   margin: 0.5rem auto;
   width: 100%;
 `
@@ -135,35 +95,26 @@ type ButtonProps = {
   isGrey?: boolean
 }
 const StyledButton = styled.div<ButtonProps>`
-  justify-content: center;
-  align-items: center;
-
   display: flex;
-
-  transition: all 0.25s;
-
+  align-items: center;
+  justify-content: center;
   border: 0.125rem solid ${({ theme }) => theme.foregroundDark};
-  border-radius: 0.25rem;
+  border-radius: 1rem;
   min-width: 2.5rem;
   max-width: 4rem;
   height: 2.5rem;
   padding: 0.25rem;
-
   background-color: ${({ theme }) => theme.background};
   cursor: pointer;
-
+  transition: all 0.25s;
   color: ${({ theme }) => theme.foreground};
-
   outline: none;
   user-select: none;
 
   &:hover {
-    transform: scale(1.15);
-
     border-color: ${({ theme }) => theme.foreground};
-
     background-color: ${({ theme }) => theme.foregroundDark};
-
+    transform: scale(1.15);
     color: ${({ theme }) => theme.background};
   }
 
@@ -173,7 +124,7 @@ const StyledButton = styled.div<ButtonProps>`
 export {
   StyledCard,
   StyledImageHolder,
-  StyledFlippingCard,
+  StyledCardImageHolder,
   StyledCardImage,
   StyledCardCount,
   StyledCardActions,
