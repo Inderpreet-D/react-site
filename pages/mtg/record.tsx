084077ca6dac67583d35d7db777594233a3cff44
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2022-04-20 23:46:24 -0600

    Starting record page

---
+++ pages/mtg/record.tsx
@@ -0,0 +1,10 @@
+import Page from '../../components/templates/Page'
+import RecordPage from '../../components/pages/Record'
+
+const Record = () => (
+  <Page title='Record'>
+    <RecordPage />
+  </Page>
+)
+
+export default Record
