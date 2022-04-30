55bd208b41459ec09a176fa063cdfaaa5e8aa77c
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2022-04-30 13:48:27 -0600

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,17 @@
 const plugins = ['tailwindcss']
 
-if (process.env.NODE_ENV === 'production') {
-  plugins.push([
-    '@fullhuman/postcss-purgecss',
-    {
-      content: [
-        './pages/**/*.{js,jsx,ts,tsx}',
-        './components/**/*.{js,jsx,ts,tsx}'
-      ],
-      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-    }
-  ])
-}
+// if (process.env.NODE_ENV === 'production') {
+//   plugins.push([
+//     '@fullhuman/postcss-purgecss',
+//     {
+//       content: [
+//         './pages/**/*.{js,jsx,ts,tsx}',
+//         './components/**/*.{js,jsx,ts,tsx}'
+//       ],
+//       defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+//     }
+//   ])
+// }
 
 plugins.push('postcss-preset-env')
 
