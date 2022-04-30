949c540c4de53608d9245aaf6dbd5a106a0a1cb0
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2022-04-30 13:54:12 -0600

    Removed purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,3 @@
-const plugins = ['tailwindcss']
-
-// if (process.env.NODE_ENV === 'production') {
-//   plugins.push([
-//     '@fullhuman/postcss-purgecss',
-//     {
-//       content: [
-//         './pages/**/*.{js,jsx,ts,tsx}',
-//         './components/**/*.{js,jsx,ts,tsx}'
-//       ],
-//       defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-//     }
-//   ])
-// }
-
-plugins.push('postcss-preset-env')
+const plugins = ['tailwindcss', 'postcss-preset-env']
 
 module.exports = { plugins }
