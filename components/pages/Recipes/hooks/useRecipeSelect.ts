ff2ac29493b8861fc7ec49f440312fc12fbc70c4
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2023-07-28 12:10:56 -0600

    Refactor to url

---
+++ components/pages/Recipes/hooks/useRecipeSelect.ts
@@ -0,0 +1,33 @@
+import { useRouter } from 'next/router'
+import { useAppDispatch } from '../../../../hooks/redux'
+
+import recipes from '../Data'
+import { reset } from '../../../../slices/recipe'
+
+const useRecipeSelect = () => {
+  const router = useRouter()
+
+  const dispatch = useAppDispatch()
+
+  const [selected, setSelected] = React.useState('')
+
+  // Select based on url
+  React.useEffect(() => {
+    if (!router.query.id) {
+      return
+    }
+
+    const key = router.query.id as string
+    if (!(key in recipes)) {
+      router.replace('/recipes')
+      return
+    }
+
+    dispatch(reset())
+    setSelected(key)
+  }, [router, dispatch])
+
+  return selected
+}
+
+export default useRecipeSelect
--- components/pages/Recipes/index.tsx
+++ components/pages/Recipes/index.tsx
@@ -1,7 +1,5 @@
 import { useRouter } from 'next/router'
 
-import { useAppDispatch } from '../../../hooks/redux'
-
 import Container from '../../atoms/Container'
 import ContainerTitle from '../../atoms/ContainerTitle'
 import RecipeBlock from './RecipeBlock'
@@ -9,43 +7,15 @@ import HorizontalList from '../../atoms/HorizontalList'
 import HorizontalListButton from '../../atoms/HorizontalListButton'
 import ContainerSectionSeparator from '../../atoms/ContainerSectionSeparator'
 
-import { reset } from '../../../slices/recipe'
-
 import recipes from './Data'
+import useRecipeSelect from './hooks/useRecipeSelect'
 
 const Page = () => {
   const router = useRouter()
 
-  const dispatch = useAppDispatch()
+  const selected = useRecipeSelect()
 
-  const [selected, setSelected] = React.useState('')
   const [hovering, setHovering] = React.useState('')
-
-  const toggleSelect = React.useCallback(
-    (name: string) => {
-      setSelected(old => {
-        if (old === name) {
-          return ''
-        }
-
-        dispatch(reset())
-        return name
-      })
-    },
-    [dispatch]
-  )
-
-  // Select based on url
-  React.useEffect(() => {
-    if (router.query.id) {
-      const key = router.query.id as string
-      if (!(key in recipes)) {
-        router.replace('/recipes')
-      } else {
-        toggleSelect(router.query.id as string)
-      }
-    }
-  }, [toggleSelect, router])
 
   return (
     <Container>
