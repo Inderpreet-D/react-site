dffe96695500355a098308224ea16cf04c21e7d4
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2023-10-13 20:13:18 -0600

    Update index.ts

--- pages/api/socket/handlers/treachery/index.ts
+++ pages/api/socket/handlers/treachery/index.ts
@@ -2,7 +2,7 @@ import { Handlers, SocketHandler } from '../../types'
 
 export const TREACHERY_WAIT = 'treachery.wait'
 
-const waitHandler: SocketHandler<any> = ({ socket, data, ack }) => {
+const waitHandler: SocketHandler<any> = ({ /*socket,*/ data, ack }) => {
   console.log('Test', { data, ack })
 
   if (ack) {
